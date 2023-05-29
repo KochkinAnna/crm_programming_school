66e2a26f02c4fbf7c5e72b31bcd0def4da1d611f
KochkinAnna <kochkinaanichka@gmail.com>
2023-05-29 21:34:04 +0300

    fixed schema

---
+++ prisma/migrations/20230529183001_group/migration.sql
@@ -0,0 +1,33 @@
+-- AlterTable
+ALTER TABLE `orders` ADD COLUMN `groupId` INTEGER NULL,
+    ADD COLUMN `roleId` ENUM('ADMIN', 'MANAGER') NULL,
+    ADD COLUMN `userId` INTEGER NULL;
+
+-- CreateTable
+CREATE TABLE `groups` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `groups_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('ADMIN', 'MANAGER') NOT NULL,
+    `firstName` VARCHAR(191) NULL,
+    `lastName` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_groupId_fkey` FOREIGN KEY (`groupId`) REFERENCES `groups`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
