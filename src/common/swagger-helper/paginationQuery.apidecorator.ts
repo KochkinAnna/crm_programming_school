1c3af4f80e44ebc787efb51acadbc6cdf87c863f
KochkinAnna <kochkinaanichka@gmail.com>
2023-06-15 20:34:02 +0300

    decorator explane

--- src/common/swagger-helper/paginationQuery.apidecorator.ts
+++ src/common/swagger-helper/paginationQuery.apidecorator.ts
@@ -24,7 +24,10 @@ export function PaginationQuery(): MethodDecorator {
       name: 'filter',
       type: String,
       example: 'course:QACX',
-      description: `Filter query in the format "field:value" for string fields (without space!).\nFor numeric fields (such as "age", "sum" and "alreadyPaid"), use the format "field:operator:value" (example: age:eq:30 or id:eq:1).\nOperators: eq (equals), neq (not equals), gt (greater than), lt (less than), gte (greater than or equal to), lte (less than or equal to)"`,
+      description: `
+   *    Filter query in the format "field:value" for string fields (without space!).
+   *    For partial string matching, use the format "field:like:value" (example: name:like:ho to receive orders with the order's name, for example Jhon).
+   *    For numeric fields (such as "age", "sum", and "alreadyPaid"), use the format "field:operator:value" (example: age:eq:30 or id:eq:1).<p>Operators: eq (equals), neq (not equals), gt (greater than), lt (less than), gte (greater than or equal to), lte (less than or equal to).</p>`,
     })(target, key, descriptor);
     ApiQuery({
       name: 'startDate',
